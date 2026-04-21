3e9941d1f1a0d3aec4ad85ecc5664a827648503e
colquenet-cloud <jref.colque.net@gmail.com>
2026-04-20 23:42:38 -0400

    arreglo de pumRoutes

--- routes/pumpRoutes.js
+++ routes/pumpRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { controlPump } from "../controllers/pumpController.js";
+import { controlPump, getPumpStatus } from "../controllers/pumpController.js"; // ← ambos
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/control", verifyToken, controlPump);
-router.get("/status", getPumpStatus); // ← sin token, ESP32 lo consulta directo
+router.get("/status", getPumpStatus); // ← sin token
 
 export default router;
